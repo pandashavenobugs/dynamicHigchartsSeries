5ec8fc03e76dca4cc5eefdb87aaddf42a7596cda
berat <cedinckan@gmail.com>
2020-10-07 20:57:12 +0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # MultipleSeries
+npm install highcharts --save
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as Highcharts from "highcharts";
+import HighchartsMore from "highcharts/highcharts-more";
+import HighchartsExporting from "highcharts/modules/exporting";
+HighchartsMore(Highcharts);
+HighchartsExporting(Highcharts);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'multiple-series';
+  title = "app";
+  chart;
+  updateFlag = false;
+  Highcharts = Highcharts;
+  chartConstructor = "chart";
+  chartCallback;
+  chartOptions = {
+    series: [
+      {
+        name:'1212',
+        data: [1, 2, 3, 6, 9],
+        type:'line',
+        color:'#f5ef42'
+      }
+    ],
+    exporting: {
+      enabled: true
+    },
+    yAxis: {
+      allowDecimals: false,
+      title: {
+        text: "Data"
+      }
+    }
+  };
+
+  constructor() {
+    const self = this;
+
+    this.chartCallback = chart => {
+      // saving chart reference
+      self.chart = chart;
+    };
+  }
+
+  ngOnInit() {
+
+  }
+
+  updateChart() {
+    this.chartOptions.series=[
+      {data:[1,2,3,4,5,6,7],
+      name:'deneme',
+      type:'area',
+    color:'#f542d1'},
+      {data:[10,20,30,40],
+        name:'deneme1',
+        type:'line',
+      color:'#42b0f5'}
+    ]
+    this.updateFlag=true
+  //   const self = this,
+  //     chart = this.chart;
+
+  //   chart.showLoading();
+  //   setTimeout(() => {
+  //     chart.hideLoading();
+
+  //     self.chartOptions.series = [
+  //       {
+  //         data: [10, 25, 15]
+  //       },
+  //       {
+  //         data: [12, 15, 10]
+  //       }
+  //     ];
+
+  //     self.chartOptions.title = {
+  //       text: "Updated title!"
+  //     };
+
+  //     self.updateFlag = true;
+  //   }, 2000);
+  }
+
+
 }
